12727a503bc7fbf6a483aa51c2d7f0cb2a5c1fdb
Pedro Del Rei <pedrodelrei@gmail.com>
2021-01-25 23:41:39 -0300

    Upload File Btn

--- SRC/frontend/src/App.vue
+++ SRC/frontend/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      </label>
+        <button v-on:click="submitFile()">Submit</button>
+    </div>
   </div>
 </template>
 
@@ -15,7 +20,7 @@ export default {
     }
   },
   mounted() {
-    this.accessAPI();
+    // this.accessAPI();
   },
   methods: {
     accessAPI(){
